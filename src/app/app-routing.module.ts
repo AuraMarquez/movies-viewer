0088dbbc22adbc058f3197953b9feb3050b0d5ac
Aura Marquez <amarquezmalpica@gmail.com>
2023-12-07 04:07:12 -0600

    add two pages with basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MovieListComponent } from './pages/movie-list/movie-list.component';
+import { MovieDetailsComponent } from './pages/movie-details/movie-details.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MovieListComponent,
+  },
+  {
+    path: 'movie',
+    component: MovieDetailsComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
