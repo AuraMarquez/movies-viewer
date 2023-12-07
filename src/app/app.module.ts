79daddce264d684360a16de9dc95dbcd7d29d1da
Aura Marquez <amarquezmalpica@gmail.com>
2023-12-07 05:26:31 -0600

    create movie thumbnail component for movie list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MovieListComponent } from './pages/movie-list/movie-list.component';
+import { MovieDetailsComponent } from './pages/movie-details/movie-details.component';
+import { MovieThumbnailComponent } from './shared/components/movie-thumbnail/movie-thumbnail.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    MovieListComponent,
+    MovieDetailsComponent,
+    MovieThumbnailComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
