abff348c49e66cae59564f487b67e630fc8ec097
Aura Marquez <amarquezmalpica@gmail.com>
2023-12-07 08:17:43 -0600

    create toogle component and movie details implementation

--- src/app/pages/movie-details/movie-details.component.ts
+++ src/app/pages/movie-details/movie-details.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { SafeUrl, DomSanitizer } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { Movie } from 'src/app/shared/interfaces/movie';
+import { WatchlistService } from 'src/app/shared/services/watchlist.service';
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.component.html',
-  styleUrls: ['./movie-details.component.css']
+  styleUrls: ['./movie-details.component.css'],
 })
 export class MovieDetailsComponent {
+  movie!: Movie;
+  trailerUrl!: SafeUrl;
 
+  constructor(
+    private router: Router,
+    private sanitizer: DomSanitizer,
+    private watchlistService: WatchlistService,
+  ) {
+    this.movie = this.router.getCurrentNavigation()?.extras.state as Movie;
+
+    this.trailerUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+      `https://www.youtube.com/embed/${this.movie.trailerLink.split('=')[1]}`,
+    );
+  }
+
+  toogleClicked() {
+    if (this.movie.onWatchlist) {
+      this.watchlistService.removeFromWatchlist(this.movie.id);
+      this.movie.onWatchlist = false;
+    } else {
+      this.watchlistService.addToWatchlist(this.movie.id);
+      this.movie.onWatchlist = true;
+    }
+  }
 }
--- src/app/shared/components/toogle/toogle.component.ts
+++ src/app/shared/components/toogle/toogle.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-toogle',
   templateUrl: './toogle.component.html',
-  styleUrls: ['./toogle.component.css']
+  styleUrls: ['./toogle.component.css'],
 })
 export class ToogleComponent {
+  @Input() data!: {
+    label: string;
+    checked: boolean;
+  };
 
+  @Output() clicked = new EventEmitter<void>();
 }
